207547db7904bfb32522228a9d124b6b9fff8531
Edaferigho <edafem@gmail.com>
2021-08-07 10:06:51 +0100

    Added cors

--- app.js
+++ app.js
@@ -1,12 +1,21 @@
 require('dotenv').config()
 const express = require('express')
 const dbConnect = require('./db');
+const cors = require('cors')
 const Tour = require('./tourModel')
 const tourRoute = require('./tourRoute')
 const data = require('./data')
 const app = express()
 
 dbConnect();
+
+const corsConfig = {
+    origin: [ "https://tourbackend.herokuapp.com/tours", "https://mytoursites.netlify.app"],
+    credentials: true,
+    methods: "GET,HEAD,OPTIONS,PUT,PATCH,POST,DELETE",
+    allowedHeaders: ['Content-Type']
+};
+app.use(cors(corsConfig));
 
 app.use('/tours',tourRoute)
 const PORT = process.env.PORT||9000
